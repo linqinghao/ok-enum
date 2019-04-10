88449aa612849c73d90dfb8a825bb8507fd7866e
Allin <linchinghao922@gmail.com>
2019-04-10 11:44:06 +0800

    fix(enum): fix enum mirror option error

--- __tests__/enum.test.js
+++ __tests__/enum.test.js
@@ -1,8 +1,14 @@
-const Enum = require('../lib/enum');
-
+const Enum = require('../lib/enum')
 
 describe('enum', () => {
-  test("knows that 2 and 2 make 4", () => {
-    expect(2 + 2).toBe(4);
-  });
+  test('Array or object can be as parameter to Enum Class ', () => {
+    const WEEK = new Enum(['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'])
+    expect(WEEK).toBeInstanceOf(Enum)
+
+    const MONTH = new Enum({
+      Jan: '1',
+      Feb: '2',
+    })
+    expect(MONTH).toBeInstanceOf(Enum)
+  })
 })
--- enumstest.js
+++ enumstest.js
@@ -1,20 +1,24 @@
 const Enum = require('./lib/enum');
 
-const dayEnum = new Enum({
-  Mon: '1',
-  Tus: '2'
-}, {
-  hooks: {
-    isMon(val) {
-      return val == this.Mon;
-    }
-  }
-});
+// const dayEnum = new Enum({
+//   Mon: '1',
+//   Tus: '2'
+// }, {
+//   hooks: {
+//     isMon(val) {
+//       return val == this.Mon;
+//     }
+//   }
+// });
 
-console.log(dayEnum);
+// console.log(dayEnum);
 
-console.log(dayEnum.isMon('123'))
+// console.log(dayEnum.isMon('123'))
 
+const Month = new Enum(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'], { mirror: true })
+
+// console.log(Month);
+console.log(Month.original())
 
 // const a = new Enum([1, 2])
 
