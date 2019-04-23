f80527575152f7b366d10038bff5033a09a659c2
Allin <linchinghao922@gmail.com>
2019-04-23 13:43:38 +0800

    test: add test

--- __tests__/enum.test.js
+++ __tests__/enum.test.js
@@ -11,4 +11,22 @@ describe('enum', () => {
     })
     expect(MONTH).toBeInstanceOf(Enum)
   })
+
+  test('instance enum with freeze config', () => {
+    const FRUIT = new Enum(['Banana', 'Apple'], { freeze: true })
+    FRUIT.Banana = '3'
+    expect(FRUIT.Banana).toEqual(0)
+  })
+
+  test('instance enum with mirror config', () => {
+    const FRUIT = new Enum(['Banana', 'Apple'], { mirror: true })
+    expect(FRUIT.values()).toEqual(['Banana', 'Apple'])
+  })
+
+  test('instance enum with handlers', () => {
+    const FRUIT = new Enum(['Banana', 'Apple'], { mirror: true, handlers: { isApple: function(val) { return val == this.Apple }} })
+    expect(FRUIT.isApple('Apple')).toEqual(true)
+    expect(FRUIT.isApple('Banana')).toEqual(false)
+  })
+
 })
